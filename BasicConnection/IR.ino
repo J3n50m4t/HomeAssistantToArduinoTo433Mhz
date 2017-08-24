b2be9348486f555bfbdc7c99db75385424134041
Markus Dittrich <markusdittrich1@gmail.com>
2017-08-24 21:14:32 +0200

    56 and 57 function code added
    turns up/down volume of stereo receiver

--- BasicConnection/IR.ino
+++ BasicConnection/IR.ino
@@ -11,5 +11,28 @@ void callcase54(){
   delay(200);
   irsend.sendSony(0xA81, 12);
   delay(200);
-  Serial.println("SonyAnlage Angeschaltet 3x send A81"); 
+  Serial.println("SonyAnlage umgeschaltet 3x send A81"); 
 }
+
+// void callcase55 == 54, because ON / OFF is a toggle on my receiver
+
+
+void callcase56(){
+  irsend.sendSony(0x481, 12);
+  delay(200);
+  irsend.sendSony(0x481, 12);
+  delay(200);
+  irsend.sendSony(0x481, 12);
+  delay(200);
+  Serial.println("SonyAnlage lauter"); 
+}
+void callcase57(){
+  irsend.sendSony(0xC81, 12);
+  delay(200);
+  irsend.sendSony(0xC81, 12);
+  delay(200);
+  irsend.sendSony(0xC81, 12);
+  delay(200);
+  Serial.println("SonyAnlage leiser"); 
+}
+
